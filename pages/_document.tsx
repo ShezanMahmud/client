288800b78a2afdbb67f269f41199220f7875c183
ShezanMahmud <shezanmahmud650@gmail.com>
2022-12-27 22:55:43 +0600

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head > 
+        <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
       </body>
     </Html>
   )
